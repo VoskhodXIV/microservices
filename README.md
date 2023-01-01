cd42b020151837ef519f84ac7aa3a4f02aafd783
Siddharth Rawat <rawat.sid@northeastern.edu>
2023-01-01 23:26:35 +0530

    :envelope: Update serverless email microservice
    Closes #5

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ This is a simple microservice that will trigger an email via Amazon SES to the u
 
 A microservice that sends verification mail to users who create a new account using the REST API for [https://prod.sydrawat.me](https://prod.sydrawat.me).
 
-Amazon SES does not trigger multiple verification e-mails to the user even if the user hits the endpoint `/v1/account/` multiple times. The verification mail is sent only once per unique user email-id.
+Amazon SES does not trigger multiple verification e-mails to the user even if the user hits the endpoint `/v2/account/` multiple times. The verification mail is sent only once per unique user email-id.
 
 The user has a 5 minute window to verify their account by clicking on the verification link sent in the email. Once verified, the user can continue using the REST API, otherwise the user should not be able to consume the REST API.
 
--- index.js
+++ index.js
@@ -1,87 +1,137 @@
-const aws = require('aws-sdk')
+const AWS = require('aws-sdk')
 
-aws.config.update({ region: 'us-east-1' })
-const ses = new aws.SES({ region: 'us-east-1' })
-const docClient = new aws.DynamoDB.DocumentClient()
+const checkSentEmail = async (
+  dynamoDBClient,
+  trackUserEmailDynamoDBTable,
+  email
+) => {
+  const params = {
+    TableName: trackUserEmailDynamoDBTable,
+    Key: {
+      Email: email,
+    },
+  }
+  const data = await dynamoDBClient.get(params).promise()
+  if (data.Item) return true
+  return false
+}
 
-const handler = async (event) => {
-  const message = event.Records[0].Sns.Message
-  const json = JSON.parse(message)
-  const email = json.username
-  const { token } = json
-  const seconds = 5 * 60
-  const secondsInEpoch = Math.round(Date.now() / 1000)
-  const expirationTime = secondsInEpoch + seconds
-
-  const table = {
-    TableName: 'csye-6225',
+const logSentEmail = async (
+  dynamoDBClient,
+  trackUserEmailDynamoDBTable,
+  email
+) => {
+  const params = {
+    TableName: trackUserEmailDynamoDBTable,
     Item: {
       Email: email,
-      Token: token,
-      TimeToLive: expirationTime,
     },
   }
+  const data = await dynamoDBClient.put(params).promise()
+  console.log('User data added', data)
+}
 
-  // Putting an item to DynamoDB Table
-  docClient.put(table, (err, data) => {
-    if (err) {
-      console.error(
-        'Unable to add item. Error JSON:',
-        JSON.stringify(err, null, 2)
-      )
-    }
+const handler = async (event, context, callback) => {
+  console.log(`Received event ${JSON.stringify(event, null, 4)}`)
+  const trackUserEmailDynamoDBTable = process.env.TrackUserEmailDynamoDBTable
+  const trackUserEmailDynamoDBRegion =
+    process.env.TrackUserEmailDynamoDBRegion || 'us-west-1'
+  const domainEnvironment = process.env.DomainEnvironment || 'prod'
+
+  console.log('Setting AWS region to:', trackUserEmailDynamoDBRegion)
+  AWS.config.update({ region: emailTrackingDynamoDBRegion })
+  const dynamoDBClient = new AWS.DynamoDB.DocumentClient({
+    region: trackUserEmailDynamoDBRegion,
   })
 
-  const mailbody = `
-        <h3>Dear CSYE User,</h3>
-        <p>
-          Thank you for registering with Network Structures and Cloud Computing coursework for this semester!
-          While we have you registered to use our REST API service, we need to verify your account.
-          </br>
-          The below link will redirect you to verify your email and allow your to consume our REST API.
-          <b>Link below link will be valid only for 5 minutes:</b>
-          </br>
-        </p>
-        <p> Verify your link:
-          <a href=https://prod.sydrawat.me/v2/verifyUserEmail?token=${token}&email=${email} >
-          https://prod.sydrawat.me/v2/verifyUserEmail?token=${token}&email=${email}</a>
-        </p>
-        <br/>
-        <p>
-          If you did not create a account with us, it is possible that someone else is trying to access our service
-          by using your account ${email}. <b>Do not forward or give this link to anyone.</b>
-        </p>
-        <br/>
-        <p>
-          You received this email because you require access to use our REST API services. If you think this is
-          incorrect, please contact our support team.
-        </p>
-        <br/>
-        Sincerely,
-        <br/>
-        The CSYE team
-        <br/>`
+  const message = event.Records[0].Sns.Message
+  const parsedMessage = JSON.parse(message)
+  console.log('Parsed message:', parsedMessage)
+  const email = parsedMessage.username
+  const { first_name, last_name, token } = parsedMessage
 
-  const params = {
-    Destination: {
-      ToAddresses: [email],
-    },
-    Message: {
-      Body: {
-        Html: {
+  const emailAlreadySent = checkSentEmail(
+    dynamoDBClient,
+    trackUserEmailDynamoDBTable,
+    email
+  )
+
+  if (!emailAlreadySent) {
+    console.log(`Sending email to ${email}`)
+    const ses = new AWS.SES()
+    const mailbody = `
+          <h3>Hello ${first_name}} ${last_name},</h3>
+          <p>
+            Thank you for registering with Network Structures and Cloud Computing coursework for this semester!
+          </p>
+          <p>
+            While we have you registered to use our REST API service, we need to verify your account.
+          </p>
+          <br>
+          <p>
+            The below link will redirect you to verify your email and allow your to consume our REST API.
+            <b>Link below link will be valid only for 5 minutes:</b>
+            <br>
+          </p>
+          <p> Verify your account:
+            <br>
+            <a href=https://${domainEnvironment}.sydrawat.me/v2/verifyUserEmail?token=${token}&email=${email} >
+            https://${domainEnvironment}.sydrawat.me/v2/verifyUserEmail?token=${token}&email=${email}</a>
+          </p>
+          <br>
+          <p>
+            If you did not create a account with us, it is possible that someone else is trying to access our service
+            by using your account ${email}. <b>Do not forward or give this link to anyone.</b>
+          </p>
+          <br>
+          <p>
+            You received this email because you require access to use our REST API services. If you think this is
+            incorrect, please contact our support team.
+          </p>
+          <br>
+          Sincerely,
+          <br/>
+          The ${domainEnvironment} CSYE team
+          <br>`
+
+    const params = {
+      Destination: {
+        ToAddresses: [email],
+      },
+      Message: {
+        Body: {
+          Html: {
+            Charset: 'UTF-8',
+            Data: mailbody,
+          },
+        },
+        Subject: {
           Charset: 'UTF-8',
-          Data: mailbody,
+          Data: `Account verification for ${domainEnvironment} CSYE6225 REST API: Action required`,
         },
       },
-      Subject: {
-        Charset: 'UTF-8',
-        Data: 'Account verification for CSYE6225: Action required',
-      },
-    },
-    Source: 'noreply@prod.sydrawat.me',
+      Source: `noreply@${domainEnvironment}.sydrawat.me`,
+    }
+    try {
+      const data = await ses.sendEmail(params).promise()
+      console.log(data)
+      console.log('Email successfully sent to:', { email })
+    } catch (err) {
+      console.log(`Could not send the email: `, err)
+    }
+    // Log this email to track sent emails to prevent duplicate verification email-link generation
+    try {
+      await logSentEmail(dynamoDBClient, trackUserEmailDynamoDBTable, email)
+      console.log('Email successfully logged to trackUserEmailDynamoDBTable')
+    } catch (err) {
+      console.log(
+        `Could not store the email to trackUserEmailDynamoDBTable: `,
+        err
+      )
+    }
+  } else {
+    console.log(`Email already sent to user: ${email}. No need to send again`)
   }
-  console.log('Email successfully sent to:', { email })
-  return ses.sendEmail(params).promise()
 }
 
 module.exports = {
